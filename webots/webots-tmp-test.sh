2375c658eb60791f9728887be54308595f7a06b1
Alex Bethel <abethel@cs.nmt.edu>
2022-11-11 16:11:14 -0700

    Add old Webots stuff

--- webots/webots-tmp-test.sh
+++ webots/webots-tmp-test.sh
@@ -3,11 +3,13 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Temporary shell script for testing Webots.
+xhost +local:root
 sudo docker run \
      --rm \
      -it \
      -e DISPLAY \
      -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
      -v /dev/dri:/dev/dri:rw \
+     -v $PWD/worlds:/webots \
      cyberbotics/webots \
      webots
