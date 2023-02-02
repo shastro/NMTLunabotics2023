56fe848b05399cdef1fd6549416d22066505e1f5
Alex Bethel <abethel@cs.nmt.edu>
2023-02-01 23:29:38 -0700

    Add installation/ubuntu.sh

---
+++ installation/ubuntu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Sets up Docker on Ubuntu, as per
+# https://docs.docker.com/engine/install/ubuntu/.
+sudo apt-get remove docker docker-engine docker.io containerd runc || true
+
+sudo apt-get update
+sudo apt-get install \
+     ca-certificates \
+     curl \
+     gnupg \
+     lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
