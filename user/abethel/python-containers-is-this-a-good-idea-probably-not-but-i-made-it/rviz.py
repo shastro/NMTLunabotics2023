0235797aea8073427ed8a314994696561690a43d
shastro <guitarshughes@hotmail.com>
2023-02-09 19:35:10 -0700

    Fixing more

--- user/abethel/python-containers-is-this-a-good-idea-probably-not-but-i-made-it/rviz.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-"""Runs RViz and connects it to ROS."""
-
-import containers
-
-# use the `pkg` argument here to require that the container have rviz
-# installed
-containers.roscmd('rviz', pkg='ros-noetic-rviz').run()
