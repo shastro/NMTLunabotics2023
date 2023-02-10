dfc26c34a245558a8960a273691164a66c793e97
Alex Bethel <abethel@cs.nmt.edu>
2023-02-09 20:52:48 -0700

    Get motor_bridge theoretically publishing

--- ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
+++ ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <unistd.h>
+
 int main(int argc, char **argv) {
-  std::cout << "Hello World!\n" << std::endl;
+  ros::init(argc, argv, "motor_bridge");
+
+  std::cout << "motor_bridge initialized" << std::endl;
+  ros::NodeHandle nh;
+  ros::Publisher publisher =
+      nh.advertise<std_msgs::String>("/publisher_test", 1000);
+
+  while (1) {
+    std_msgs::String s;
+    s.data = "hello";
+    publisher.publish(s);
+    sleep(1);
+  }
 }
