e201406e3bedabaed539c27cbc50723cd5c7ddb8
shastro <guitarshughes@hotmail.com>
2023-02-11 22:58:39 -0700

    Canbus updates

---
+++ can/canbed/send/send.ino
@@ -0,0 +1,48 @@
+// send a frame from can bus
+
+#include <SPI.h>
+#include "mcp_can.h"
+
+#include <mcp_can.h>
+#include <SPI.h>
+
+const int SPI_CS_PIN = 17;              // CANBed V1
+// const int SPI_CS_PIN = 3;            // CANBed M0
+// const int SPI_CS_PIN = 9;            // CAN Bus Shield
+
+MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+
+void setup()
+{
+    Serial.begin(115200);
+    while(!Serial);
+    while (CAN_OK != CAN.begin(CAN_500KBPS))    // init can bus : baudrate = 500k
+    {
+        Serial.println("CAN BUS FAIL!");
+        delay(100);
+    }
+    Serial.println("CAN BUS OK!");
+}
+
+unsigned char stmp[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+void loop()
+{
+    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
+    stmp[7] = stmp[7]+1;
+    if(stmp[7] == 100)
+    {
+        stmp[7] = 0;
+        stmp[6] = stmp[6] + 1;
+        
+        if(stmp[6] == 100)
+        {
+            stmp[6] = 0;
+            stmp[5] = stmp[6] + 1;
+        }
+    }
+    
+    CAN.sendMsgBuf(0x00, 0, 8, stmp);
+    delay(100);                       // send data per 100ms
+}
+
+// END FILE
