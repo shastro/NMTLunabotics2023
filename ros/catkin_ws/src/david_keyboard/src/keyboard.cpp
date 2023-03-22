f21924774660e6edd6bd5c31ad664389b39ae0a5
Niall Devlin <niallcdevlin@gmail.com>
2023-03-21 20:48:07 -0600

    Fixed pitch message

---
+++ ros/catkin_ws/src/david_keyboard/src/keyboard.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "david_keyboard");
+
+  std::cout << "david keyboard initialized" << std::endl;
+  ros::NodeHandle nh;
+  ros::Publisher publisher =
+      nh.advertise<std_msgs::String>("/publisher_test", 1000);
+
+  while (1) {
+    std_msgs::String s;
+    s.data = "hello";
+    publisher.publish(s);
+    sleep(1);
+  }
+}
