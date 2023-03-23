28c2f64571a0e260e8062ca1e17ad789ec8a32ae
Alex Bethel <abethel@cs.nmt.edu>
2023-03-22 22:20:07 -0600

    Encode CAN messages in pitchCallback

--- ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
+++ ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
@@ -10,29 +10,45 @@
 #include "can/david.h"
 
 static struct {
-    const int HOME_LENGTH = 0;
-    const int MAX_LENGTH = 1024;
-    int target_length; // set by callback.
-    int actual_length; // actual length as estimated by telemetry.
-    const int FORWARD = 0;
-    const int BACKWARD = 1;
-    int motor_direction;
-    int motor_on;
+  const int HOME_LENGTH = 0;
+  const int MAX_LENGTH = 1024;
+  int target_length; // set by callback.
+  int actual_length; // actual length as estimated by telemetry.
+  const int FORWARD = 0;
+  const int BACKWARD = 1;
+  int motor_direction;
+  int motor_on;
 } PitchMotorState;
 
-void pitchCallback(const motor_bridge::Pitch::ConstPtr& msg) {
-    int length = msg->length;
-    std::cout << "Pitch Message Received. length: " << length << std::endl;
+void pitchCallback(const motor_bridge::Pitch::ConstPtr &msg) {
+  int length = msg->length;
+  std::cout << "Pitch Message Received. length: " << length << std::endl;
+
+  david_pitch_ctrl_left_t left = {
+    .count = (uint64_t)length,
+    .tolerance = 100,
+  };
+
+  david_pitch_ctrl_right_t right = {
+    .count = (uint64_t)length,
+    .tolerance = 100,
+  };
+
+  uint8_t message[8];
+  david_pitch_ctrl_left_pack(message, &left, sizeof(message));
+  // TODO: send message here.
+
+  david_pitch_ctrl_right_pack(message, &right, sizeof(message));
+  // TODO: send message here.
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "motor_bridge");
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("pitch_control", 1024, pitchCallback);
 
   // Callback event loop
   ros::spin();
-  
+
   return 0;
 }
