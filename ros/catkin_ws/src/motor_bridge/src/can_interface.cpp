a09abe86cc835846f8238f73e317ec19894c1842
Alex Bethel <abethel@cs.nmt.edu>
2023-03-23 21:48:53 -0600

    Start writing can_interface.cpp

---
+++ ros/catkin_ws/src/motor_bridge/src/can_interface.cpp
@@ -0,0 +1,62 @@
+#include <cstring>
+#include <iostream>
+#include <linux/can.h>
+#include <net/if.h>
+#include <string>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "can_interface.hpp"
+
+using namespace std;
+
+fd::fd() : id(-1) {}
+fd::fd(int n) : id(n) {}
+fd::fd(fd &&other) {
+    id = other.id;
+    other.id = -1;
+}
+fd::~fd() {
+    if (id != -1)
+        close(id);
+}
+
+void fd::operator=(fd &&other) {
+    id = other.id;
+    other.id = -1;
+}
+int fd::operator*() { return id; }
+
+int fd::into_raw() {
+    int id_ = id;
+    id = -1;
+    return id_;
+}
+
+SocketCAN::SocketCAN(const string &interface) {
+    sock = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (*sock == -1)
+        throw string("Error creating socket");
+
+    // Select CAN interface.
+    ifreq req;
+    strncpy(req.ifr_ifrn.ifrn_name, interface.data(),
+            sizeof(req.ifr_ifrn.ifrn_name));
+    if (ioctl(*sock, SIOCGIFINDEX, &req) == -1)
+        // TODO: leaks a socket
+        throw string("Unable to select CAN interface");
+
+    // Bind the socket to the network interface.
+    sockaddr_can addr;
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = req.ifr_ifru.ifru_ivalue;
+    if (bind(*sock, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == -1)
+        throw string("Failed to bind socket to network interface");
+
+    cout << "Successfully bound socket to interface." << endl;
+}
+
+void SocketCAN::transmit(const can_frame &cf) {
+    // todo
+}
