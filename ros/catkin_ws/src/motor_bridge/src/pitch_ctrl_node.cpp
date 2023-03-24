d5649822a2e9baa5b37a6dd9275c7ebadf62706a
Niall Devlin <niallcdevlin@gmail.com>
2023-03-23 19:54:10 -0600

    fixed pich control node

--- ros/catkin_ws/src/motor_bridge/src/pitch_ctrl_node.cpp
+++ ros/catkin_ws/src/motor_bridge/src/pitch_ctrl_node.cpp
@@ -10,45 +10,37 @@
 #include "can/david.h"
 
 static struct {
-  const int HOME_LENGTH = 0;
-  const int MAX_LENGTH = 1024;
-  int target_length; // set by callback.
-  int actual_length; // actual length as estimated by telemetry.
-  const int FORWARD = 0;
-  const int BACKWARD = 1;
-  int motor_direction;
-  int motor_on;
+    const int HOME_LENGTH = 0;
+    const int MAX_LENGTH = 1024;
+    int target_length; // set by callback.
+    int actual_length; // actual length as estimated by telemetry.
+    const int FORWARD = 0;
+    const int BACKWARD = 1;
+    int motor_direction;
+    int motor_on;
 } PitchMotorState;
 
 void pitchCallback(const motor_bridge::Pitch::ConstPtr &msg) {
-  int length = msg->length;
-  std::cout << "Pitch Message Received. length: " << length << std::endl;
+    int length = msg->length;
+    std::cout << "Pitch Message Received. length: " << length << std::endl;
 
-  david_pitch_ctrl_left_t left = {
-    .count = (uint64_t)length,
-    .tolerance = 100,
-  };
+    david_pitch_ctrl_both_t both = {
+        .count = (uint64_t)length,
+        .tolerance = 100,
+    };
 
-  david_pitch_ctrl_right_t right = {
-    .count = (uint64_t)length,
-    .tolerance = 100,
-  };
-
-  uint8_t message[8];
-  david_pitch_ctrl_left_pack(message, &left, sizeof(message));
-  // TODO: send message here.
-
-  david_pitch_ctrl_right_pack(message, &right, sizeof(message));
-  // TODO: send message here.
+    uint8_t message[8];
+    david_pitch_ctrl_both_pack(message, &both, sizeof(message));
+    //TODO: send message
 }
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "pitch_ctrl_node");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("pitch_control", 1024, pitchCallback);
+    ros::init(argc, argv, "pitch_ctrl_node");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("pitch_control", 1024, pitchCallback);
 
-  // Callback event loop
-  ros::spin();
+    // Callback event loop
+    ros::spin();
 
-  return 0;
+    return 0;
 }
