0182569301a55d12a9f0e1a7301fca221c63a7dd
Alex Bethel <abethel@cs.nmt.edu>
2023-03-23 21:17:35 -0600

    Add can_interface.hpp

---
+++ ros/catkin_ws/src/motor_bridge/src/can_interface.hpp
@@ -0,0 +1,34 @@
+#ifndef CAN_INTERFACE_H
+#define CAN_INTERFACE_H
+
+#include <linux/can.h>
+
+// Basic file descriptor wrapper. Not really intended for use outside
+// SocketCAN.
+#include <string>
+class fd {
+    int id;
+
+  public:
+    fd();
+    fd(int n);
+    fd(fd &&other);
+    ~fd();
+
+    void operator=(fd &&other);
+    int operator*();
+
+    int into_raw();
+};
+
+// CAN socket.
+class SocketCAN {
+    fd sock;
+
+  public:
+    SocketCAN(const std::string &interface);
+
+    void transmit(can_frame cf);
+};
+
+#endif /* CAN_INTERFACE_H */
