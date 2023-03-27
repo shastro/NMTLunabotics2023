399e72c85f88c0bf383378b1ebf0229fcb5086c0
dev_dwarf <logan.c.forman@gmail.com>
2023-03-27 16:35:26 -0600

    flashing stepper

--- can/canbed/stepper_ctrl/stepper_ctrl.ino
+++ can/canbed/stepper_ctrl/stepper_ctrl.ino
@@ -1,16 +1,23 @@
 #include <Stepper.h>
 #include <Wire.h>
 
+#include "mcp_can.h"
+#include "david.h"
+
 const int stepsPerRevolution = 700;  // change this to fit the number of steps per revolution
 const int RPM = 1;
 const int stepsPerTick = 10;
 
+enum PINS {
+    PUL1 = 4,
+    DIR1 = 11,
+    PUL2 = 10,
+    DIR2 = 5,
+    SPI_CS_PIN = 17
+};
 
-// initialize the stepper library on pins 8 through 11:
-#define PUL1 4
-#define DIR1 11
-#define PUL2 10
-#define DIR2 5
+MCP_CAN CAN(SPI_CS_PIN); // Set CS pin
+
 Stepper stepper1(stepsPerRevolution, PUL1, DIR1);
 Stepper stepper2(stepsPerRevolution, PUL2, DIR2);
 
@@ -35,41 +42,43 @@ void setup() {
 
 void loop() {
 
-  // Check for new messages
-  if(CAN_MSGAVAIL == CAN.checkReceive()) {
-      // read data
-      CAN.readMsgBuf(&len, buf);
-      uint32_t canId = CAN.getCanId();
+    // Check for new messages
+    if(CAN_MSGAVAIL == CAN.checkReceive()) {
+        unsigned char len = 0;
+        unsigned char buf[8] = {0};
+        // read data
+        CAN.readMsgBuf(&len, buf);
+        uint32_t canId = CAN.getCanId();
 
-      // IF ESTOP stop other behaviors
-      if (canId == DAVID_E_STOP_FRAME_ID) {
-          estopped = true;
-      }
+        // IF ESTOP stop other behaviors
+        if (canId == DAVID_E_STOP_FRAME_ID) {
+            estopped = true;
+        }
 
-      if (canId == DAVID_STEPPER_CTRL_FRAME_ID) {
-          move_dir = extract_value(buf, 0, 1);              
-      }
-  }
+        if (canId == DAVID_STEPPER_CTRL_FRAME_ID) {
+            move_dir = extract_value(buf, 0, 1);              
+        }
+    }
 
-  // step one step:
-  if (!estopped) {
-      if (move_dir == FORWARD) {
-          stepper1.step(-stepsPerTick);
-          stepper2.step(stepsPerTick);
-      } else if (move_dir == BACKWARD) {
-          stepper1.step(stepsPerTick);
-          stepper2.step(-stepsPerTick);
-      }
-  }
+    // step one step:
+    if (!estopped) {
+        if (move_dir == FORWARD) {
+            stepper1.step(-stepsPerTick);
+            stepper2.step(stepsPerTick);
+        } else if (move_dir == BACKWARD) {
+            stepper1.step(stepsPerTick);
+            stepper2.step(-stepsPerTick);
+        }
+    }
 
 }
 
 int64_t extract_value(char buf[], int first_byte, int bytes) {
-  int64_t count = 0;
-  int64_t temp = 0;
-  for (int i = first_byte; i< first_byte+bytes; i++) {
-    temp = (int64_t)buf[i];
-    count += temp << (8*i);
-  }
-  return count;  
+    int64_t count = 0;
+    int64_t temp = 0;
+    for (int i = first_byte; i< first_byte+bytes; i++) {
+        temp = (int64_t)buf[i];
+        count += temp << (8*i);
+    }
+    return count;  
 }
