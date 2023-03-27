46f36a768de7419a2f1c5622af4c5b192f4663d2
shastro <guitarshughes@hotmail.com>
2023-03-27 16:13:33 -0600

    Add pitch simple control

--- can/canbed/pitch_ctrl_simple/pitch_ctrl_simple.ino
+++ can/canbed/pitch_ctrl_simple/pitch_ctrl_simple.ino
@@ -108,16 +108,11 @@ void setup()
 	delay(100);
     
   // PinModes
-  pinMode(PIN_TABLE[MOTOR_LEFT][HALL], INPUT_PULLUP);
   pinMode(PIN_TABLE[MOTOR_LEFT][P1], OUTPUT);
   pinMode(PIN_TABLE[MOTOR_LEFT][P2], OUTPUT);
-  pinMode(PIN_TABLE[MOTOR_RIGHT][HALL], INPUT_PULLUP);
   pinMode(PIN_TABLE[MOTOR_RIGHT][P1], OUTPUT);
   pinMode(PIN_TABLE[MOTOR_RIGHT][P2], OUTPUT);
 
-  // Interrupts
-  attachInterrupt(digitalPinToInterrupt(PIN_TABLE[MOTOR_LEFT][HALL]), left_hall_handler, FALLING);
-  attachInterrupt(digitalPinToInterrupt(PIN_TABLE[MOTOR_RIGHT][HALL]), right_hall_handler, FALLING);
 
   Serial.begin(115200);
   while (CAN_OK != CAN.begin(CAN_500KBPS))    // init can bus : baudrate = 500k
@@ -135,108 +130,54 @@ void loop()
   unsigned char buf[8] = {0};
   
 // Check for new messages
-  if ((left_dir == STOP) && (right_dir == STOP)) {
-	  if(CAN_MSGAVAIL == CAN.checkReceive()) {
-	    // read data
-	    CAN.readMsgBuf(&len, buf);
-	    uint32_t canId = CAN.getCanId();
+  if(CAN_MSGAVAIL == CAN.checkReceive()) {
+  // read data
+    CAN.readMsgBuf(&len, buf);
+    uint32_t canId = CAN.getCanId();
 
-	    if (canId == DAVID_E_START_FRAME_ID) {
-	        estopped = false;
-	    }
-    
-	    // IF ESTOP stop other behaviors
-	    if (canId == DAVID_E_STOP_FRAME_ID) {
-	        estopped = true;
-	    }
+    if (canId == DAVID_E_START_FRAME_ID) {
+        estopped = false;
+    }
 
-	    if (canId == DAVID_SET_TRIG_DELAY_FRAME_ID) {
-	        trig_delay = extract_value(buf, 0, 6);
-	        max_count = extract_value(buf, 6, 2);
-	    }
+    // IF ESTOP stop other behaviors
+    if (canId == DAVID_E_STOP_FRAME_ID) {
+        estopped = true;
+    }
 
-	    // Enter homing state
-	    if (!estopped && !homing) {    
-	        if (canId == DAVID_PITCH_CTRL_HOME_FRAME_ID) {
-	            homing = true;
-	            left_target_count = 0;
-	            right_target_count = 0;
-	            left_prev_count = left_true_count+1;
-	            right_prev_count = right_true_count+1;
-	        }
-	    }
-    
-	    // Set lengths
-	    if (!estopped && !homing) {    
-	        // If PITCH_CTRL_BOTH both if statements will fire.
-	        if (canId == DAVID_PITCH_CTRL_BOTH_FRAME_ID) {
-	            left_target_count = extract_value(buf, 0, 6);
-	            right_target_count = left_target_count;
-	            left_tolerance = extract_value(buf, 6, 2);
-	            right_tolerance = left_tolerance;
-	        }
-	        if (canId == DAVID_PITCH_CTRL_LEFT_FRAME_ID) {
-	            left_target_count = extract_value(buf, 0, 6);
-	            left_tolerance = extract_value(buf, 6, 2);
-	        }
-	        if (canId == DAVID_PITCH_CTRL_RIGHT_FRAME_ID) {
-	            right_target_count = extract_value(buf, 0, 6);
-	            right_tolerance = extract_value(buf, 6, 2);
-	        }
-	        if (left_target_count > max_count) {
-	          left_target_count = max_count;
-	        }
-	        if (right_target_count > max_count) {
-	          right_target_count = max_count;
-	        }
-	    }
+    if (canId == DAVID_SET_TRIG_DELAY_FRAME_ID) {
+        trig_delay = extract_value(buf, 0, 6);
+        max_count = extract_value(buf, 6, 2);
+    }
 
-	    // Print CAN message
-	    Serial.println("-----------------------------");
-	    Serial.println(canId, HEX);
+    // Enter homing state
 
-	  } // finish CAN message
-  }
-  // Do homing
-  if (!estopped && homing) {
-      left_dir = RETRACT;
-      right_dir = RETRACT;
-      
-      if ((left_true_count == left_prev_count)
-          && (right_true_count == right_prev_count)) {
-          count_stationary_timer++;
-      } else {
-          count_stationary_timer = 0;
-      }
-      left_prev_count = left_true_count;
-      right_prev_count = right_true_count;
+    // Set lengths
+    // If PITCH_CTRL_BOTH both if statements will fire.
+    if (canId == DAVID_PITCH_CTRL_BOTH_FRAME_ID) {
+        left_dir = extract_value(buf, 0, 6);
+        right_dir = left_dir;
+    }
+    if (canId == DAVID_PITCH_CTRL_LEFT_FRAME_ID) {
+        left_dir = extract_value(buf, 0, 6);
+    }
+    if (canId == DAVID_PITCH_CTRL_RIGHT_FRAME_ID) {
+        right_dir = extract_value(buf, 0, 6);
+    }
 
-      if (count_stationary_timer > homing_delay) {
-          homing = false;
-          left_true_count = 0; 
-          right_true_count = 0;
-          left_target_count = 0;
-          right_target_count = 0;
-      }
- } else {
+    // Print CAN message
+    Serial.println("-----------------------------");
+    Serial.println(canId, HEX);
+
+  } // finish CAN message
 		
-  // set_control(MOTOR_LEFT, EXTEND);
-  // set_control(MOTOR_RIGHT, EXTEND);
-}
   
 // Standard movement towards target counts
-
- if (!estopped && !homing) {
-     left_dir = get_direction(left_true_count, left_target_count, left_tolerance);
-     right_dir = get_direction(right_true_count, right_target_count, right_tolerance);
- }
-  
   set_control(MOTOR_LEFT, left_dir);
   set_control(MOTOR_RIGHT, right_dir);
 
   // Send telemetry
-  send_telemetry(DAVID_PITCH_TELEM_LEFT_FRAME_ID, left_true_count, left_dir);
-  send_telemetry(DAVID_PITCH_TELEM_RIGHT_FRAME_ID, right_true_count, right_dir);
+  send_telemetry(DAVID_PITCH_TELEM_LEFT_FRAME_ID, left_dir, left_dir);
+  send_telemetry(DAVID_PITCH_TELEM_RIGHT_FRAME_ID, right_dir, right_dir);
 
 }
 
@@ -253,53 +194,6 @@ void send_telemetry(uint32_t canId, int64_t true_count, int64_t dir){
   CAN.sendMsgBuf(canId, 0, 8, buf);
 }
 
-enum DIR get_direction(int64_t true_count, int64_t target_count, int64_t tolerance) {
-  enum DIR retdir = STOP; // Should be stop
-  // Serial.println(abs((int)true_count - (int)target_count));
-  // Serial.print("target_count: ");
-  // Serial.println((int)target_count);
-  if (abs(true_count - target_count) >= DEFAULT_TOLERANCE) {
-    if (true_count > target_count) {
-      retdir = RETRACT;  
-    } else {
-      retdir = EXTEND;  
-    }
-  }
-  
-  return retdir;
-}
-
-void left_hall_handler(){
-  if ((micros() - left_last_trigger) > trig_delay) {
-	  left_last_trigger = micros();
-	  switch (left_dir) {
-	    case RETRACT: 
-	      left_true_count -= 1;
-	      break;
-	    case EXTEND: 
-	      left_true_count += 1;
-	      break;
-	    case STOP: 
-	      break;
-	  }
-  } 
-
-}
-void right_hall_handler(){
-  if ((micros() - right_last_trigger) > trig_delay) {
-	  right_last_trigger = micros();
-	  switch (right_dir) {
-	    case RETRACT: 
-	      right_true_count -= 1;
-	      break;
-	    case EXTEND: 
-	      right_true_count += 1;
-	      break;
-	    case STOP: 
-	      break;
-	  }
-  }
-}
 
 int64_t extract_value(char buf[], int first_byte, int bytes) {
   int64_t count = 0;
