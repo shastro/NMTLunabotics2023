9ed0b74b284101eb5a8d3d2ee3bcd4f253cd12d9
Alex Bethel <abethel@cs.nmt.edu>
2023-03-27 21:02:26 -0600

    Formatting

--- ros/catkin_ws/src/david_tui/src/tui.cpp
+++ ros/catkin_ws/src/david_tui/src/tui.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <ncurses.h>
 
-#include <motor_bridge/Pitch.h>
 #include <motor_bridge/Drive.h>
+#include <motor_bridge/Pitch.h>
 #include <motor_bridge/Stepp.h>
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <string>
 #include <unistd.h>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 #define print_bold(...)                                                        \
     do {                                                                       \
@@ -18,21 +18,20 @@
         attroff(A_BOLD);                                                       \
     } while (0)
 
-
 enum motor { BOTH = 0, LEFT = 1, RIGHT = 2 };
-enum control { PITCH = 0, LOCO = 1, STEPP = 2};
-enum direction { BACKWARD = -1, STOP = 0, FORWARD = 1};
+enum control { PITCH = 0, LOCO = 1, STEPP = 2 };
+enum direction { BACKWARD = -1, STOP = 0, FORWARD = 1 };
 
 static std::unordered_map<char, std::string> bindings = {
-    //Control
+    // Control
     {'p', "Pitch"},
     {'d', "Drive"},
     {'a', "Excavation Arm"},
-    //Motor
+    // Motor
     {'b', "Both"},
     {'l', "Left"},
     {'r', "Right"},
-    //Command
+    // Command
     {'j', "Drive Forward"},
     {'k', "Drive Backward"},
     {'J', "Pitch Up"},
@@ -41,8 +40,7 @@ static std::unordered_map<char, std::string> bindings = {
     {'l', "Arm In"},
     {'s', "Stop Current"},
     {'c', "Stop All"},
-    {'q', "Quit"}
-};
+    {'q', "Quit"}};
 
 static ros::Publisher pitch_pub;
 static ros::Publisher loco_pub;
@@ -114,7 +112,6 @@ int main(int argc, char **argv) {
     // Refresh at 0.1-second interval.
     halfdelay(1);
 
-
     while (true) {
         clear();
         move(0, 0);
@@ -124,130 +121,132 @@ int main(int argc, char **argv) {
         int n = getch();
 
         switch (n) {
-            case 'p':
-                sel_c = PITCH;
-                break;
-            case 'd':
-                sel_c = LOCO;
-                break;
-            case 'e':
-                sel_c = STEPP;
+        case 'p':
+            sel_c = PITCH;
+            break;
+        case 'd':
+            sel_c = LOCO;
+            break;
+        case 'e':
+            sel_c = STEPP;
 
-            case 'l':
-                sel_m = LEFT;
-                break;
-            case 'r':
-                sel_m = RIGHT;
-                break;
-            case 'b':
-                sel_m = BOTH;
-                if (sel_c == PITCH) {
-                    motorsys.pitch.length.left = motorsys.pitch.length.right;
-                } else if (sel_c == LOCO) {
-                    motorsys.loco.speed.left = motorsys.loco.speed.right;
-                } else if (sel_c == STEPP) {
-                    motorsys.stepp.rpm.left = motorsys.stepp.rpm.right;
-                    motorsys.stepp.dir.left = motorsys.stepp.dir.right;
-                }
-                break;
+        case 'l':
+            sel_m = LEFT;
+            break;
+        case 'r':
+            sel_m = RIGHT;
+            break;
+        case 'b':
+            sel_m = BOTH;
+            if (sel_c == PITCH) {
+                motorsys.pitch.length.left = motorsys.pitch.length.right;
+            } else if (sel_c == LOCO) {
+                motorsys.loco.speed.left = motorsys.loco.speed.right;
+            } else if (sel_c == STEPP) {
+                motorsys.stepp.rpm.left = motorsys.stepp.rpm.right;
+                motorsys.stepp.dir.left = motorsys.stepp.dir.right;
+            }
+            break;
 
-            case 'j':
-                //TODO implement changeable speed
-                sel_c = LOCO;
-                if (sel_m != RIGHT) {
-                    motorsys.loco.speed.left = motorsys.loco.speed.max;
-                } else if (sel_m != LEFT) {
-                    motorsys.loco.speed.right = motorsys.loco.speed.max;
-                }
-                break;
-            case 'k':
-                sel_c = LOCO;
-                if (sel_m != RIGHT) {
-                    motorsys.loco.speed.left = motorsys.loco.speed.min;
-                } else if (sel_m != LEFT) {
-                    motorsys.loco.speed.right = motorsys.loco.speed.min;
-                }
-                break;
-            case 'J':
-                sel_c = PITCH;
-                if (sel_m != RIGHT) {
-                    motorsys.pitch.length.left = std::min(motorsys.pitch.length.left + 64,
-                            motorsys.pitch.length.max);
-                } else if (sel_m != LEFT) {
-                    motorsys.pitch.length.right = std::min(motorsys.pitch.length.right + 64,
-                            motorsys.pitch.length.max);
-                }
-                break;
-            case 'K':
-                sel_c = PITCH;
-                if (sel_m != RIGHT) {
-                    motorsys.pitch.length.left = std::max(motorsys.pitch.length.left - 64,
-                            motorsys.pitch.length.min);
-                } else if (sel_m != LEFT) {
-                    motorsys.pitch.length.right = std::max(motorsys.pitch.length.right - 64,
-                            motorsys.pitch.length.min);
-                }
-                break;
-            case 'o':
-                sel_c = STEPP;
-                if (sel_m != RIGHT) {
-                    motorsys.stepp.rpm.left = motorsys.stepp.rpm.max;
-                    motorsys.stepp.dir.left = FORWARD;
-                } else if (sel_m != LEFT) {
-                    motorsys.stepp.rpm.right = motorsys.stepp.rpm.max;
-                    motorsys.stepp.dir.right = FORWARD;
-                }
-                break;
-            case 'i':
-                sel_c = STEPP;
-                if (sel_m != RIGHT) {
-                    motorsys.stepp.rpm.left = motorsys.stepp.rpm.max;
-                    motorsys.stepp.dir.left = BACKWARD;
-                } else if (sel_m != LEFT) {
-                    motorsys.stepp.rpm.right = motorsys.stepp.rpm.max;
-                    motorsys.stepp.dir.right = BACKWARD;
-                }
-                break;
+        case 'j':
+            // TODO implement changeable speed
+            sel_c = LOCO;
+            if (sel_m != RIGHT) {
+                motorsys.loco.speed.left = motorsys.loco.speed.max;
+            } else if (sel_m != LEFT) {
+                motorsys.loco.speed.right = motorsys.loco.speed.max;
+            }
+            break;
+        case 'k':
+            sel_c = LOCO;
+            if (sel_m != RIGHT) {
+                motorsys.loco.speed.left = motorsys.loco.speed.min;
+            } else if (sel_m != LEFT) {
+                motorsys.loco.speed.right = motorsys.loco.speed.min;
+            }
+            break;
+        case 'J':
+            sel_c = PITCH;
+            if (sel_m != RIGHT) {
+                motorsys.pitch.length.left = std::min(
+                    motorsys.pitch.length.left + 64, motorsys.pitch.length.max);
+            } else if (sel_m != LEFT) {
+                motorsys.pitch.length.right =
+                    std::min(motorsys.pitch.length.right + 64,
+                             motorsys.pitch.length.max);
+            }
+            break;
+        case 'K':
+            sel_c = PITCH;
+            if (sel_m != RIGHT) {
+                motorsys.pitch.length.left = std::max(
+                    motorsys.pitch.length.left - 64, motorsys.pitch.length.min);
+            } else if (sel_m != LEFT) {
+                motorsys.pitch.length.right =
+                    std::max(motorsys.pitch.length.right - 64,
+                             motorsys.pitch.length.min);
+            }
+            break;
+        case 'o':
+            sel_c = STEPP;
+            if (sel_m != RIGHT) {
+                motorsys.stepp.rpm.left = motorsys.stepp.rpm.max;
+                motorsys.stepp.dir.left = FORWARD;
+            } else if (sel_m != LEFT) {
+                motorsys.stepp.rpm.right = motorsys.stepp.rpm.max;
+                motorsys.stepp.dir.right = FORWARD;
+            }
+            break;
+        case 'i':
+            sel_c = STEPP;
+            if (sel_m != RIGHT) {
+                motorsys.stepp.rpm.left = motorsys.stepp.rpm.max;
+                motorsys.stepp.dir.left = BACKWARD;
+            } else if (sel_m != LEFT) {
+                motorsys.stepp.rpm.right = motorsys.stepp.rpm.max;
+                motorsys.stepp.dir.right = BACKWARD;
+            }
+            break;
 
-            case 's':
-                if (sel_c == LOCO) {
-                    motorsys.loco.speed.left = 0;
-                    motorsys.loco.speed.right = 0;
-                } else if (sel_c == STEPP) {
-                    motorsys.stepp.rpm.left = 0;
-                    motorsys.stepp.rpm.right = 0;
-                    motorsys.stepp.dir.left = STOP;
-                    motorsys.stepp.dir.right = STOP;
-                } else if (sel_c == PITCH) {
-                    motorsys.pitch.length.left = motorsys.pitch.length.max;
-                    motorsys.pitch.length.right = motorsys.pitch.length.max;
-                }
-                break;
-            case 'c':
+        case 's':
+            if (sel_c == LOCO) {
                 motorsys.loco.speed.left = 0;
                 motorsys.loco.speed.right = 0;
+            } else if (sel_c == STEPP) {
                 motorsys.stepp.rpm.left = 0;
                 motorsys.stepp.rpm.right = 0;
                 motorsys.stepp.dir.left = STOP;
                 motorsys.stepp.dir.right = STOP;
+            } else if (sel_c == PITCH) {
                 motorsys.pitch.length.left = motorsys.pitch.length.max;
                 motorsys.pitch.length.right = motorsys.pitch.length.max;
-                break;
-            case 'q':
-                motorsys.loco.speed.left = 0;
-                motorsys.loco.speed.right = 0;
-                motorsys.stepp.rpm.left = 0;
-                motorsys.stepp.rpm.right = 0;
-                motorsys.stepp.dir.left = STOP;
-                motorsys.stepp.dir.right = STOP;
-                motorsys.pitch.length.left = motorsys.pitch.length.max;
-                motorsys.pitch.length.right = motorsys.pitch.length.max;
-                endwin();
-                return 0;
+            }
+            break;
+        case 'c':
+            motorsys.loco.speed.left = 0;
+            motorsys.loco.speed.right = 0;
+            motorsys.stepp.rpm.left = 0;
+            motorsys.stepp.rpm.right = 0;
+            motorsys.stepp.dir.left = STOP;
+            motorsys.stepp.dir.right = STOP;
+            motorsys.pitch.length.left = motorsys.pitch.length.max;
+            motorsys.pitch.length.right = motorsys.pitch.length.max;
+            break;
+        case 'q':
+            motorsys.loco.speed.left = 0;
+            motorsys.loco.speed.right = 0;
+            motorsys.stepp.rpm.left = 0;
+            motorsys.stepp.rpm.right = 0;
+            motorsys.stepp.dir.left = STOP;
+            motorsys.stepp.dir.right = STOP;
+            motorsys.pitch.length.left = motorsys.pitch.length.max;
+            motorsys.pitch.length.right = motorsys.pitch.length.max;
+            endwin();
+            return 0;
 
-            default:
-                // do nothing
-                break;
+        default:
+            // do nothing
+            break;
         }
         send_targets();
     }
@@ -264,7 +263,7 @@ static void send_targets() {
 
 static void send_pitch(motor m) {
     motor_bridge::Pitch msg;
-    msg.motor = (int) m;
+    msg.motor = (int)m;
     if (m == RIGHT) {
         msg.length = motorsys.pitch.length.right;
     } else {
@@ -275,7 +274,7 @@ static void send_pitch(motor m) {
 
 static void send_loco(motor m) {
     motor_bridge::Drive msg;
-    msg.motor = (int) m;
+    msg.motor = (int)m;
     if (m == RIGHT) {
         msg.speed = motorsys.loco.speed.right;
     } else {
@@ -286,7 +285,7 @@ static void send_loco(motor m) {
 
 static void send_stepp(motor m) {
     motor_bridge::Stepp msg;
-    msg.motor = (int) m;
+    msg.motor = (int)m;
     if (m == RIGHT) {
         msg.rpm = motorsys.stepp.rpm.right;
         msg.direction = motorsys.stepp.dir.right;
