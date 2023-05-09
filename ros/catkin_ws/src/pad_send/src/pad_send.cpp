4eed583a33d368d84a346479d45e8acdcbee9054
Niall Devlin <niallcdevlin@gmail.com>
2023-05-08 22:25:16 -0600

    Slowwing down can

--- ros/catkin_ws/src/pad_send/src/pad_send.cpp
+++ ros/catkin_ws/src/pad_send/src/pad_send.cpp
@@ -17,7 +17,7 @@ int main(int argc, char *argv[]) {
     int min = -max;
     int dead = 20;
     ros::Publisher pub = nh.advertise<motor_bridge::System>("/system", 5);
-    ros::Rate loop_rate(1);
+    ros::Rate rate(10);
 
     // Setup ncurses
     initscr();
@@ -172,6 +172,7 @@ int main(int argc, char *argv[]) {
         refresh();
 
         pub.publish(s);
+        rate.sleep();
     }
     endwin();
     return 0;
