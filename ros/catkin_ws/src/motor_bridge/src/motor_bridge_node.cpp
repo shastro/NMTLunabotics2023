12c285542b5cde2d87edc5a6e074b06a15d03d4c
Niall Devlin <niallcdevlin@gmail.com>
2023-05-09 14:02:38 -0600

    testing some motor bridge

--- ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
+++ ros/catkin_ws/src/motor_bridge/src/motor_bridge_node.cpp
@@ -11,7 +11,7 @@
 #include "can_interface.hpp"
 #include <linux/can.h>
 
-#define CAN_BUS "can0"
+#define CAN_BUS "vcan0"
 static SocketCAN can;
 
 motor_bridge::System lastMsg;
@@ -30,6 +30,7 @@ int main(int argc, char **argv) {
         halfdelay(1);
         start_color();
         init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(2, COLOR_WHITE, COLOR_BLACK);
 
         can = SocketCAN(CAN_BUS);
 
@@ -46,57 +47,71 @@ int main(int argc, char **argv) {
     }
 }
 
+void print(std::string s, int color) {
+    attron(COLOR_PAIR(color));
+    printw(s.c_str());
+    attroff(COLOR_PAIR(color));
+    refresh();
+}
+
+void print(const char* s, int color) {
+    attron(COLOR_PAIR(color));
+    printw(s);
+    attroff(COLOR_PAIR(color));
+    refresh();
+}
+
 void callback(const motor_bridge::System::ConstPtr &msg) {
-    if (lastMsg == *msg) { return; }
     erase();
     move(0, 0);
 
-    uint8_t buff[8];
-    int can_id;
-
-    can_id = pack_msg(msg->e_stop, buff);
-    printw(printable(msg->e_stop).c_str());
-    send(buff, can_id, "EStop");
-
-    can_id = pack_msg(msg->pitch_ctrl, buff);
-    printw(printable(msg->pitch_ctrl).c_str());
-    send(buff, can_id, "Pitch");
-
-    can_id = pack_msg(msg->loco_ctrl, buff);
-    printw(printable(msg->loco_ctrl).c_str());
-    //send(buff, can_id, "Loco");
-    david_loco_ctrl_t t {
-        .left_vel = msg->loco_ctrl.left_vel,
-        .right_vel = msg->loco_ctrl.right_vel,
-    };
-    david_loco_ctrl_pack(buff, &t, sizeof(t));
-    can.transmit(DAVID_LOCO_CTRL_FRAME_ID, buff);
-
-    can_id = pack_msg(msg->excav_ctrl, buff);
-    printw(printable(msg->excav_ctrl).c_str());
-    send(buff, can_id, "Excav");
-
-    can_id = pack_msg(msg->stepper_ctrl, buff);
-    printw(printable(msg->stepper_ctrl).c_str());
-    send(buff, can_id, "Stepper");
-
-    refresh();
+    e_stop();
+    pitch_ctrl();
 
     lastMsg = *msg;
 }
 
-void send(uint8_t* buff, int id, const char* name) {
+void e_stop() {
+    if (msg->e_stop == lastMsg.e_stop) { return; }
+    
+    int id = DAVID_E_STOP_FRAME_ID;
+    uint8_t buff[8];
+    david_e_stop_t t {
+        .stop = msg->e_stop.stop,
+    };
+    david_e_stop_pack(buff, &t, sizeof(t));
+
     try {
         can.transmit(id, buff);
+        print(printable(msg->e_stop), 1);
+        refresh();
     } catch (std::string err) {
-        attron(COLOR_PAIR(1));
-        printw(name);
-        printw(": ");
-        printw(err.c_str());
-        printw("\n");
+        print("EStop: ", 2);
+        print(err, 2);
+        print("\n", 2);
+    }
+}
+
+void pitch_ctrl() {
+    if (msg->pitch_ctrl == lastMsg.pitch_ctrl) { return; }
+
+    int id = DAVID_PTICH_CTRL_FRAME_ID;
+    uint8_t buff[8];
+    david_pitch_ctrl_t t {
+        .set_point = msg->pitch_ctrl.set_point;
+        .left_offset = msg->pitch_ctrl.left_offset;
+        .right_offset = msg->pitch_ctrl.right_offset;
+    };
+    david_pitch_ctrl_pack(buff, &t, sizeof(t));
+
+    try {
+        can.transmit(id, buff);
+        print(printable(msg->pitch_ctrl), 1);
         refresh();
-        attroff(COLOR_PAIR(1));
-        refresh();
+    } catch (std::string err) {
+        print("Pitch Ctrl: ", 2);
+        print(err, 2);
+        print("\n", 2);
     }
 }
 
