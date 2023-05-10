0ff1fcdf53cc85e8a90a4f275409647c9bd6a193
shastro <guitarshughes@hotmail.com>
2023-05-09 22:15:03 -0600

    Recopy all the arduino_lib

--- can/canbed/stepper_ctrl/arduino_lib.hpp
+++ can/canbed/stepper_ctrl/arduino_lib.hpp
@@ -57,17 +57,16 @@ inline CANPacket can_read(MCP_CAN &can) {
     if (can.checkReceive() == CAN_MSGAVAIL) {
         CANPacket packet;
         packet.len = 0;
-        packet.id = 0xFFFFFFFFFFF;
+        packet.id = 0xFFFFFFFF;
         can.readMsgBuf((unsigned char *)&packet.len, packet.buf);
         packet.id = can.getCanId();
         return packet;
     } else {
         CANPacket packet;
         packet.len = 8;
-        packet.id = 0xFFFFFFFFFFF;
+        packet.id = 0xFFFFFFFF;
         return packet;
     }
-
 }
 
 inline void can_send(MCP_CAN &can, CANPacket packet) {
@@ -90,8 +89,11 @@ class OutPin {
 class InPin {
     int num;
     float threshold;
+
   public:
-    InPin(int num, float threshold = 0.8f) : num(num), threshold(threshold) { pinMode(num, INPUT); }
+    InPin(int num, float threshold = 0.8f) : num(num), threshold(threshold) {
+        pinMode(num, INPUT);
+    }
     bool read() { return digitalRead(num) == HIGH; }
     float read_analog() { return (analogRead(num) / 1023.0); }
     bool read_threshold() { return read_analog() >= threshold; }
@@ -120,6 +122,57 @@ struct Relay {
         left.write(true);
         right.write(false);
         return common;
+    }
+};
+
+// Single Stepper Motor
+struct Stepper {
+    OutPin pulse;
+    OutPin direction;
+
+    Stepper(int pulse_pin, int dir_pin)
+        : pulse(pulse_pin), direction(dir_pin) {}
+
+    void doStep(unsigned int step) {
+        const int pulse_sequence[] = {HIGH, HIGH, LOW, LOW};
+        const int dir_sequence[] = {LOW, HIGH, HIGH, LOW};
+        pulse.write(pulse_sequence[step % 4]);
+        direction.write(dir_sequence[step % 4]);
+    }
+};
+
+// Motor controller object.
+class MidwestMotorController {
+    OutPin enable;
+    Relay relay;
+
+  public:
+    MidwestMotorController(int inhibit, int relay_ccw, int relay_cw,
+                    int relay_pwm)
+        : enable(inhibit), relay(relay_ccw, relay_cw, relay_pwm) {
+        setVel(0);
+    }
+
+    const double vel_scale = 100.0;
+    const double vel_deadzone = 0.01;
+    void setVel(double vel) {
+        vel /= vel_scale;
+        // Experimentally determined that the motors' maximum velocity
+        // is somewhere around 0.45-0.5 duty cycle. It's unclear why,
+        // but as a result for better control we can just multiply by
+        // 0.5;
+        vel *= 0.5;
+
+        // The motors are slightly more responsive when going forwards
+        // than when going backwards.
+        if (vel < 0)
+            vel *= 1.5;
+
+        enable.write(!(abs(vel) > vel_deadzone));
+        if (vel > 0)
+            relay.output_right().write_pwm(abs(vel));
+        else
+            relay.output_left().write_pwm(abs(vel));
     }
 };
 
