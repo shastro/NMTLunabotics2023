b4050f6d001891fb7598cfe8cb996fc18fc4677c
Alex Bethel <abethel@cs.nmt.edu>
2023-05-15 20:15:24 -0600

    I got rid of the raw pointers look at meeee
    it theoretically gets soundly garbage collected by the destructors :3

--- ros/catkin_ws/src/david_config/src/pcl_pub.cpp
+++ ros/catkin_ws/src/david_config/src/pcl_pub.cpp
@@ -1,39 +1,39 @@
-#include <ros/ros.h>
 #include <ros/param.h>
+#include <ros/ros.h>
 #include <sensor_msgs/point_cloud_conversion.h>
 
 class PosePub {
     ros::Publisher pub;
 
-public:
-    PosePub(ros::NodeHandle &nh, std::string topic) :
-        pub(nh.advertise<sensor_msgs::PointCloud>(topic, 10)) {}
+  public:
+    PosePub(ros::NodeHandle &nh, std::string topic)
+        : pub(nh.advertise<sensor_msgs::PointCloud>(topic, 10)) {}
 
-    void callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+    void callback(const sensor_msgs::PointCloud2::ConstPtr &msg) {
         sensor_msgs::PointCloud new_msg;
         sensor_msgs::convertPointCloud2ToPointCloud(*msg, new_msg);
         pub.publish(new_msg);
     }
 };
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     ros::init(argc, argv, "pose_pub");
     ros::NodeHandle nh;
-    
+
     std::vector<std::string> names;
     ros::param::getParamNames(names);
-    
+
     // Hey beanie check out this fucked shit
+    std::vector<std::pair<ros::Subscriber, std::unique_ptr<PosePub>>> subs;
     for (auto &name : names) {
         std::string t_sub;
-        ros::param::get(name, topic)
-        std::string t_pub = t_sub.substr(0, t_pub.find("/", 1));
-        t_pub += "/pointcloud";
-        
-        PosePub* pub = new PosePub(nh, topic);
-        ros::Subscriber* sub = new ros::Subscriber(
-            nh.subscribe(topic, 10, &PosePub::callback, pub));
+        ros::param::get(name, t_sub);
+        std::string t_pub = t_sub.substr(0, t_sub.find('/')) + "/pointcloud";
+
+        std::unique_ptr<PosePub> pub(new PosePub(nh, t_pub));
+        subs.push_back(
+            std::make_pair(nh.subscribe(t_sub, 10, &PosePub::callback, &*pub),
+                           std::move(pub)));
     }
 
     ros::spin();
