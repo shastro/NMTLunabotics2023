fbc1c48bd49bc67c94e34c9a72a18293121588f7
dev_dwarf <logan.c.forman@gmail.com>
2023-05-22 08:31:44 -0600

    stop forgetting output modes

--- can/canbed/stepper_sanity/stepper_sanity.ino
+++ can/canbed/stepper_sanity/stepper_sanity.ino
@@ -5,47 +5,36 @@
 #include "arduino_lib.hpp"
 
 void setup() {
-#define RPUL 10
-#define RDIR 6
-#define LPUL 4
-#define LDIR 11
-#define MIN_LIMIT A0
-#define MAX_LIMIT A1
-
-#define DIR_BACKWARD HIGH
-#define DIR_FORWARD LOW
-
+#define BACKWARD 1
+#define FORWARD 0
 #define DELAY 50
 
-    pinMode(RPUL, OUTPUT);
-    pinMode(RDIR, OUTPUT);
-    pinMode(LPUL, OUTPUT);
-    pinMode(LDIR, OUTPUT);
-
-    InPin min_limit(MIN_LIMIT);
+    OutPin rpul(10);
+    OutPin rdir(6);
+    OutPin lpul(4);
+    OutPin ldir(11);
     
-    digitalWrite(RDIR, DIR_FORWARD);
-    digitalWrite(LDIR, DIR_FORWARD);
+    InPin min_limit(A0);
 
+    rdir.write(FORWARD);
+    ldir.write(FORWARD);
     for (int i = 0; i < 20000; i++) {
-        digitalWrite(RPUL, LOW);
-        digitalWrite(LPUL, LOW);
-        digitalWrite(RPUL, HIGH);
-        digitalWrite(LPUL, HIGH);
+        rpul.write(0);
+        lpul.write(0);
+        rpul.write(1);
+        lpul.write(1);
         delayMicroseconds(DELAY);
     }
 
-    digitalWrite(RDIR, DIR_BACKWARD);
-    digitalWrite(LDIR, DIR_BACKWARD);
-
-   while (!min_limit.read()) {
-        digitalWrite(RPUL, LOW);
-        digitalWrite(LPUL, LOW);
-        digitalWrite(RPUL, HIGH);
-        digitalWrite(LPUL, HIGH);
+    rdir.write(BACKWARD);
+    ldir.write(BACKWARD);
+    while (!min_limit.read()) {
+        rpul.write(0); 
+        lpul.write(0);
+        rpul.write(1);
+        lpul.write(1);
         delayMicroseconds(DELAY);
-   }
-
+    }
 
     for (;;) {}
 }
