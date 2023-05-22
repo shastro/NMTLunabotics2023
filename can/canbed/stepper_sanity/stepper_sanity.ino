e59bc807ff7e44a2c58a72bfbc88253cace72c32
dev_dwarf <logan.c.forman@gmail.com>
2023-05-22 08:23:06 -0600

    restore sanity

--- can/canbed/stepper_sanity/stepper_sanity.ino
+++ can/canbed/stepper_sanity/stepper_sanity.ino
@@ -15,29 +15,36 @@ void setup() {
 #define DIR_BACKWARD HIGH
 #define DIR_FORWARD LOW
 
-    //InPin min_limit(MIN_LIMIT);
+#define DELAY 50
+
+    pinMode(RPUL, OUTPUT);
+    pinMode(RDIR, OUTPUT);
+    pinMode(LPUL, OUTPUT);
+    pinMode(LDIR, OUTPUT);
+
+    InPin min_limit(MIN_LIMIT);
     
     digitalWrite(RDIR, DIR_FORWARD);
     digitalWrite(LDIR, DIR_FORWARD);
 
-    for (int i = 0; i < 40000; i++) {
+    for (int i = 0; i < 20000; i++) {
         digitalWrite(RPUL, LOW);
         digitalWrite(LPUL, LOW);
         digitalWrite(RPUL, HIGH);
         digitalWrite(LPUL, HIGH);
-        delayMicroseconds(50);
+        delayMicroseconds(DELAY);
     }
 
     digitalWrite(RDIR, DIR_BACKWARD);
     digitalWrite(LDIR, DIR_BACKWARD);
 
-   // while (!min_limit.read()) {
+   while (!min_limit.read()) {
         digitalWrite(RPUL, LOW);
         digitalWrite(LPUL, LOW);
         digitalWrite(RPUL, HIGH);
         digitalWrite(LPUL, HIGH);
-        delayMicroseconds(50);
-  //  }
+        delayMicroseconds(DELAY);
+   }
 
 
     for (;;) {}
