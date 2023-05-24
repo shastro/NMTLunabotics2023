0754790b3fd2c4f99cd3d9cd4e971a1b69eb962a
Niall Devlin <niallcdevlin@gmail.com>
2023-05-24 18:56:40 -0500

    unfuck logan code

--- ros/catkin_ws/src/david_navigation/src/occupancy_grid.cpp
+++ ros/catkin_ws/src/david_navigation/src/occupancy_grid.cpp
@@ -46,7 +46,7 @@ void callback(const grid_map_msgs::GridMap::ConstPtr& msg) {
         for (int i = 0; i < nRows; i++) {
             float norm_val = mx.coeff(i,j)/range;
             float diff = norm_val - norm_median;
-            grid.data[j + i*nCols] = 100.0*diff*diff; 
+            grid.data[i + j*nCols] = 100.0*diff*diff*diff; 
         }
     }
 
