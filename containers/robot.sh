786ee8c3e44217241c77f0cd079d2643efe6fb2f
Alex Bethel <abethel@cs.nmt.edu>
2023-05-24 10:03:19 -0500

    Use decimate filter with correct syntax

--- containers/robot.sh
+++ containers/robot.sh
@@ -109,6 +109,7 @@ docker exec -d $CONTAINER_NAME /ros_entrypoint.sh \
     roslaunch realsense2_camera rs_camera.launch \
     camera:=d455_1 serial_no:=213522250920 \
     filters:=pointcloud depth_fps:=30 enable_color:=false \
+    decimate_filter.enable:=true \
     depth_width:=640 \
     depth_height:=480 \
     pointcloud_texture_stream:=RS2_STREAM_ANY
@@ -119,6 +120,7 @@ docker exec -d $CONTAINER_NAME /ros_entrypoint.sh \
     roslaunch realsense2_camera rs_camera.launch \
     camera:=d455_2 serial_no:=213522253528 \
     filters:=pointcloud depth_fps:=30 enable_color:=false \
+    decimate_filter.enable:=true \
     depth_width:=640 \
     depth_height:=480 \
     pointcloud_texture_stream:=RS2_STREAM_ANY
@@ -128,6 +130,7 @@ sleep 5
 docker exec -d $CONTAINER_NAME /ros_entrypoint.sh \
     roslaunch realsense2_camera rs_camera.launch \
     camera:=l515_1 serial_no:=f1381818 \
+    decimate_filter.enable:=true \
     depth_width:=640 \
     depth_height:=480 \
     filters:=pointcloud depth_fps:=30 enable_color:=false \
@@ -138,6 +141,7 @@ sleep 5
 docker exec -d $CONTAINER_NAME /ros_entrypoint.sh \
     roslaunch realsense2_camera rs_camera.launch \
     camera:=l515_2 serial_no:=f0461308 \
+    decimate_filter.enable:=true \
     depth_width:=640 \
     depth_height:=480 \
     filters:=pointcloud depth_fps:=30 enable_color:=false \
