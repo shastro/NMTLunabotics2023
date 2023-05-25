cfba27a71aef4963ba71ed9e448141242c578c5b
dev_dwarf <logan.c.forman@gmail.com>
2023-05-25 09:05:17 -0600

    Update occupancy_grid.cpp

--- ros/catkin_ws/src/david_navigation/src/occupancy_grid.cpp
+++ ros/catkin_ws/src/david_navigation/src/occupancy_grid.cpp
@@ -58,10 +58,10 @@ void callback(const grid_map_msgs::GridMap::ConstPtr& msg) {
     for (int j = 0; j < nCols; j++) {
         for (int i = 0; i < nRows; i++) {
             float norm_val = (mx.coeff(i,j) - min)/range;
-            norm_val = isnan(norm_val) ? norm_median : norm_val;
-            float diff = norm_val - norm_median;
+            // norm_val = isnan(norm_val) ? norm_median : norm_val;
+            // float diff = norm_val - norm_median;
             // grid.data[(nRows-1-i) + nRows*(nCols-1-j)] = 100.0*(diff > final_threshold);
-            grid.data[(nRows-1-i) + nRows*(nCols-1-j)] = 100.0*abs(diff);
+            grid.data[(nRows-1-i) + nRows*(nCols-1-j)] = 100.0*abs(norm_val);
         }
     }
 
